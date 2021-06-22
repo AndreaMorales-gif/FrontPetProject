1ac10e7f1fbc20fb5a6b5286f2ccdeb79f7e1988
Andrea Morales <andmorales11@ielasnieves.edu.co>
2021-06-21 23:09:03 -0500

    Se hizó formulario con todo lo establecido.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [id, setId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [entryDate, setEntryDate] = useState("");
+  const save = ()=>{
+  fetch('http://localhost:8080/api/'+id+'/'+userId+'/'+entryDate+'', {method: 'POST'})
+  .then(response => response.json())
+  .then(data=>console.log(data));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="bg-img">
+  <form action="/action_page.php" className="container">
+    <h1>Registro</h1>
+
+    <label for="id"><b>Id</b></label>
+    <input type="text" placeholder="Ingrese su id" onChange={(e)=> setId(e.target.value)} name="id" required value={id}/>
+
+    <label for="userId"><b>Documento Identidad</b></label>
+    <input type="text" placeholder="Ingrese su Cédula" onChange={(e)=> setUserId(e.target.value)} name="userId" required value={userId}/>
+
+    <label for="entryDate"><b>Fecha </b></label>
+    <input type="date" placeholder="Ingrese su Cédula" onChange={(e)=> setEntryDate(e.target.value)} name="entryDate" value={entryDate}/>
+
+    <button type="submit" value="Guardar" className="btn" onClick={save}>Listo</button>
+  </form>
+</div>
   );
 }
 
